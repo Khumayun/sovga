505765e4b6d804a33088c98d5a45e13761015165
Khumayun <huba4buba@gmail.com>
2019-04-14 15:50:15 +0500

    fixed clash filters with paginator

--- products/views.py
+++ products/views.py
@@ -1,26 +1,23 @@
-
 from django.views.generic import View
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
-
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def product_list_view(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    paginator = Paginator(products,8) # Show 16 products per page
-
-    page = request.GET.get('page')
-    paged_products = paginator.get_page(page)
-
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category)
 
+    paginator = Paginator(products,8) # Show 16 products per page
+
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+    
     context = {
         'categories': categories,
         'products': paged_products
